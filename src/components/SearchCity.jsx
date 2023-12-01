f7690bfea0a0399e2d0bd714aab63c6c4424899d
IsoHovhannisyan <143291832+IsoHovhannisyan@users.noreply.github.com>
2023-12-01 18:38:37 +0400

    Chagned searchcity Name

--- src/components/SearchCity.jsx
+++ src/components/SearchCity.jsx
@@ -1,6 +1,6 @@
 
 import { useState, useEffect, useRef } from 'react'
-import '../css/searchcity.css';
+import '../css/SearchCity.css';
 
 export function SearchCity({cities, selectedCity, setSelectedCity,bookingPageLabel}) {
 
